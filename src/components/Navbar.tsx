dafbe1f44f351ec6b1678b619360576d461e774d
Kieran <kieranlarson203@gmail.com>
2024-01-27 01:18:47 -0600

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Button from './Button'
+
+function Navbar() {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const dropDownBar = () =>{
+    setIsVisible(!isVisible)
+  }
+
+  const clicked = () => {
+    setIsVisible(false)
+  }
+
+  return(
+    <nav className='flex items-center justify-between flex-wrap bg-blue-500 p-4'>
+      <div className='flex items-center flex-shrink-0 text-grey mr-5'>
+        <Link to='/' className='font-bold text-xl tracking-tight text-white'>Check Out Cars!</Link>
+      </div>
+      <div className="block">
+        <button onClick={dropDownBar}
+          className='flex items-center px-4 py-1 
+          text-gray-600 item-center rounded border 
+          hover:text-red-600'>
+          <i className="fa-solid fa-bars"></i>
+          </button>
+      </div>
+      { isVisible ?(
+      <div className="w-full block flex-grow items-start rounded-xl">
+        <div className='text-md lg:text-grow'>
+          <Button className='p-2 m-5 bg-blue-700 justify-center shadow-xl rounded'>
+            <div>
+              <Link to='/' className='flex place-content-center mt-2 lg:inline-block lg:mt-1
+              text-gray-300 hover:text-red-600 mr-2 '>Home</Link>
+            </div>
+          </Button>
+          <Button className='p-2 m-5 bg-blue-700 justify-center shadow-xl rounded'>
+            <div>
+              <Link to='/about' onClick={ clicked } className='flex place-content-center mt-2 lg:inline-block lg:mt-1
+              text-gray-300 hover:text-red-600 shadow-xl'>
+                About
+              </Link>
+            </div>
+          </Button>
+          <Button className='p-2 m-5 bg-blue-700 justify-center shadow-xl rounded'>
+            <div>
+              <Link to='/dashboard' onClick={ clicked } className='flex place-content-center mt-2 lg:inline-block lg:mt-1
+              text-gray-300 hover:text-red-600'>
+                Cars To See
+              </Link>
+            </div>
+          </Button>
+          </div>
+      </div> 
+      ):(
+        <></>
+      )}
+    </nav>
+  )
+}
+export default Navbar
